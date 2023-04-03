5446a772d2fa447ecaf0168555efa8502cad93e4
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-03 15:10:33 +0700

    Membuat struct AlamatDetail dan Mahasiswa

--- ActivityStructure3_160/ActivityStructure3_160.cpp
+++ ActivityStructure3_160/ActivityStructure3_160.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+struct ALamatDetail {
+    char desa[20];
+    char kota[20];
+};
+
+struct Mahasiswa {
+    char NIM[12];
+    char nama[20];
+    ALamatDetail alamat;
+    int umur;
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
