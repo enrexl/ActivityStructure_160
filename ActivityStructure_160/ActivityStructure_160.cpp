e2770d610613edff9f1d381bedb34964ab4120fb
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-03 14:38:54 +0700

    Membuat Cout dari struct mhs1 dan mhs2

--- ActivityStructure_160/ActivityStructure_160.cpp
+++ ActivityStructure_160/ActivityStructure_160.cpp
@@ -8,14 +8,31 @@ struct Mahasiswa {
     int umur;
 };
 
-
 int main()
 {
     Mahasiswa mhs1, mhs2;
     mhs1.NIM = "20220140160";
     mhs1.nama = "Jordy";
     mhs1.alamat = "Kasihan";
-    mhs1.umur = 20
+    mhs1.umur = 20;
 
-    
+    cout << "Masukkan NIM : ";
+    cin >> mhs2.NIM;
+    cout << "Masukkan Nama : ";
+    cin >> mhs2.nama;
+    cout << "Masukkan Alamat : ";
+    cin >> mhs2.alamat;
+    cout << "Masukkan Umur : ";
+    cin >> mhs2.umur;
+
+    cout << "\nNIM : " << mhs1.NIM;
+    cout << "\nNama : " << mhs1.nama;
+    cout << "\nAlamat : " << mhs1.alamat;
+    cout << "\nUmmur : " << mhs1.umur;
+
+    cout << "\nNIM : " << mhs2.NIM;
+    cout << "\nNama : " << mhs2.nama;
+    cout << "\nAlamat : " << mhs2.alamat;
+    cout << "\nUmur : " << mhs2.umur;
+
 }
